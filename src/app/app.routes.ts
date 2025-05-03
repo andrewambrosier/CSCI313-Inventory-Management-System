4e3011b6262319b960d4269434fab656bf7d5fad
Fargauche <benjamin.norling@ndsu.edu>
2025-05-03 15:42:52 -0500

    Login component added and routing updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 import { InventoryComponent } from './components/inventory/inventory.component';
 
 export const routes: Routes = [
-    {path: '', component: InventoryComponent}, // This sets home page
+  { path: '', redirectTo: 'login', pathMatch: 'full' },  // Redirect empty path to login
+  { path: 'login', component: LoginComponent },
+  { path: 'inventory', component: InventoryComponent }
 ];
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
-import { USERS } from '../services/mock-users';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';   
+import { FormsModule } from '@angular/forms';       
+import { USERS } from '../../services/mock-users';
 
 @Component({
   selector: 'app-login',
+  standalone: true,                                 
+  imports: [CommonModule, FormsModule],            
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
@@ -12,11 +17,14 @@ export class LoginComponent {
   error = '';
   isLoggedIn = false;
 
+  constructor(private router: Router) {}
+
   login() {
-    const user = USERS.find(u => u.username === this.username && u.password === this.password);
+    const user = USERS.find((u: any) => u.username === this.username && u.password === this.password);
     if (user) {
       this.isLoggedIn = true;
       this.error = '';
+      this.router.navigate(['/inventory']);
     } else {
       this.error = 'Invalid username or password';
     }
